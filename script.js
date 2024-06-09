8105959045e50bd08abaeea8620bb27fbcd1aada
Emiel Mac <112609864+EKlijnsma@users.noreply.github.com>
2024-06-09 15:26:18 +0200

    add size setting functionality
    clicking the button the user can provide a new size of the grid
    all current tiles are removed and a new grid is added

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-const grid = 16
 const cont = document.querySelector(".container")
+const sizeBtn = document.querySelector("#size")
 
-for (let i = 0; i < grid; i++) {
-    const row = document.createElement("div")
-    row.classList.add("row")
+resetGrid(16)
 
-    for (let j = 0; j < grid; j++) {
-        // create tiles
-        // append tiles as child to row container
-        const tile = document.createElement("div")
-        tile.classList.add("tile")
-        row.appendChild(tile)
-        tile.addEventListener("mouseover", (event) => {event.target.classList.add("black")});
+function resetGrid(size) {
+    deleteTiles();
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div")
+        row.classList.add("row")
+
+        for (let j = 0; j < size; j++) {
+            // create tiles
+            // append tiles as child to row container
+            const tile = document.createElement("div")
+            tile.classList.add("tile")
+            row.appendChild(tile)
+            tile.addEventListener("mouseover", (event) => {event.target.classList.add("black")});
+        }
+        // append row container as child to container
+        cont.appendChild(row)
     }
-    // append row container as child to container
-    cont.appendChild(row)
+}
+
+sizeBtn.addEventListener("click", getGridSize)
+
+function getGridSize() {
+    let size = 0;
+    while (size > 100 || size < 1) {
+        size = prompt("Enter grid size (max 100): ")
+    }
+    resetGrid(size)
+}
+
+function deleteTiles() {
+    const tiles = document.querySelectorAll(".tile")
+    for (tile of tiles) {
+        tile.remove()
+    }
 }
